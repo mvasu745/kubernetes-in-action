dce8ddef1d1d4200aa54161aaac3d988382709c6
Manickavasagam Arunachalam <149649477+mvasu745@users.noreply.github.com>
2024-01-28 18:55:11 -0500

    Update app.js

--- Chapter02/kubia/app.js
+++ Chapter02/kubia/app.js
@@ -6,9 +6,8 @@ console.log("Kubia server starting...");
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("You've hit " + os.hostname() + "\n");
+  response.end("You've hit " + os.hostname() + "\n"+ "Updated Version");
 };
 
 var www = http.createServer(handler);
 www.listen(8080);
-
